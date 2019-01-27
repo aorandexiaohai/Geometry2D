346c08b76c0323c9481852d22d982981cd274046
wzh <2663976081@qq.com>
2019-01-27 22:41:49 +0800

    code

--- point.h
+++ point.h
@@ -3,12 +3,21 @@ namespace G2D
 {
 class Point
 {
-  private:
+  public:
     double x = 0.0;
     double y = 0.0;
-  Point(double xx, double yy){
-      x = xx;
-      y = yy;
-  }
+    Point(double xx = 0, double yy = 0)
+    {
+        x = xx;
+        y = yy;
+    }
+    Point operator+(const Point &other)const
+    {
+        return {x + other.x, y + other.y};
+    }
+    Point operator-(const Point &other)const
+    {
+        return {x - other.x, y - other.y};
+    }
 };
 } // namespace G2D
