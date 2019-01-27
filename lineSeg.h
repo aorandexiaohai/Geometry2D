d27f2bd18e94fe8fa2538204fec42515efa00b66
wzh <2663976081@qq.com>
2019-01-27 22:53:09 +0800

    code:

--- lineSeg.h
+++ lineSeg.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "point.h"
 #include <cmath>
+#include <cassert>
 namespace G2D
 {
 class LineSeg
@@ -13,15 +14,34 @@ class LineSeg
     LineSeg(Point bb = Point{}, Point ee = Point{}) : beg(bb), end(ee)
     {
     }
-    double Length() const
+    double length() const
     {
         return std::sqrt((beg.x - end.x) * (beg.x - end.x) + (beg.y - end.y) * (beg.y - end.y));
     }
-    double Cross(const LineSeg &ls) const
+    double cross(const LineSeg &ls) const
     {
         auto v1 = end - beg;
         auto v2 = ls.end - ls.beg;
         return (v1.x*v2.y - v1.y*v2.x);
+    }
+    //连续线段p0p1 p1p2
+    //this代表:p0p1
+    //ls代表:p1p2
+    //p1p2相对于p0p1,在p1处是否左转
+    //结果:
+    //1:左转
+    //0:共线
+    //-1:右转
+    int rotateLeft(const LineSeg& ls) const
+    {
+        //必须相等
+        assert(this->end == ls.beg);
+
+        auto indicator = this->cross(ls);
+        if(indicator < 0) return -1;
+        else if (indicator>0) return 1;
+        return 0;
+
     }
 };
 } // namespace G2D
--- point.h
+++ point.h
@@ -11,13 +11,25 @@ class Point
         x = xx;
         y = yy;
     }
-    Point operator+(const Point &other)const
+    Point operator+(const Point &other) const
     {
         return {x + other.x, y + other.y};
     }
-    Point operator-(const Point &other)const
+    Point operator-(const Point &other) const
     {
         return {x - other.x, y - other.y};
+    }
+    bool operator<(const Point &pt) const
+    {
+        if (x < pt.x)
+            return true;
+        else if (x == pt.x)
+            return y < pt.y;
+        return false;
+    }
+    bool operator==(const Point &pt) const
+    {
+        return (!(*this < pt)) && (!(pt < *this));
     }
 };
 } // namespace G2D
